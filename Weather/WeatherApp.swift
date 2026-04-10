cd234b62aac3ced2e3321116ce75302698392515
bekiosi12-boop <bekiosi12@icloud.com>
2026-04-10 11:18:54 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Osmanov's macbook on 4/10/26.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
